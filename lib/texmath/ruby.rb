0c24703ab5ba4e33758e19dbd496bf9b13321d77
Casper Hollingberry <hollingberry7@gmail.com>
2015-01-14 22:23:05 -0800

    Initial commit

---
+++ lib/texmath/ruby.rb
@@ -0,0 +1,7 @@
+require "texmath/ruby/version"
+
+module Texmath
+  module Ruby
+    # Your code goes here...
+  end
+end
---
+++ lib/texmath/ruby/version.rb
@@ -0,0 +1,5 @@
+module Texmath
+  module Ruby
+    VERSION = "0.0.1"
+  end
+end
