73e2d63c7129f1ce302d59fec4ee1c0aacdc79b7
Casper Hollingberry <hollingberry7@gmail.com>
2015-02-18 20:28:15 -0800

    Clean up error handling
    * Use ArgumentError instead of InvalidFormatError, InvalidReaderError,
      and InvalidWriterError. Much simpler.
    * Pass the error message to Kernel#raise instead of creating a new
      instance of the errors. A little bit cleaner.

--- lib/texmath/errors.rb
+++ lib/texmath/errors.rb
@@ -7,16 +7,4 @@ module TeXMath
   ##
   # Error raised when the `texmath` executable can't be found.
   class NoExecutableError < StandardError; end
-
-  ##
-  # Error raised when an invalid reader or writer format is specified.
-  class InvalidFormatError < StandardError; end
-
-  ##
-  # Error raised when an invalid reader format is specified.
-  class InvalidReaderError < InvalidFormatError; end
-
-  ##
-  # Error raised when an invalid writer format is specified.
-  class InvalidWriterError < InvalidFormatError; end
 end
